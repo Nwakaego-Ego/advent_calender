c989b79bbf5a580c7babbf38f31f2f721a3d714c
Ego Nwaekpe <glorynwaekpe@gmail.com>
2023-12-25 14:04:31 -0800

    useContext

--- src/app/Quote/quote.jsx
+++ src/app/Quote/quote.jsx
@@ -1,8 +1,55 @@
+// // import axios from "axios";
+// // import { useState } from "react";
+
+// // const useQuote = () => {
+// //   const [quote, setQuote] = useState(null);
+// //   const [error, setError] = useState(null);
+
+// //   const fetchQuote = async () => {
+// //     try {
+// //       const response = await axios.get("https://api.quotable.io/random");
+// //       setQuote(response?.data);
+// //       console.log(response.data);
+// //     } catch (error) {
+// //       setError("Error fetching quote");
+// //     }
+// //   };
+// //   return { quote, fetchQuote };
+// // };
+
+// // export default useQuote;
+
+// import axios from "axios";
+// import { useState } from "react";
+
+// export const useQuote = () => {
+//   const [quote, setQuote] = useState(null);
+//   const [error, setError] = useState(null);
+
+//   const fetchQuote = async () => {
+//     try {
+//       const response = await axios.get("https://api.quotable.io/random");
+//       setQuote(response?.data);
+//       console.log(response.data);
+//     } catch (error) {
+//       setError("Error fetching quote");
+//     }
+//   };
+
+//   return { quote, fetchQuote };
+// };
+
+// export const Quote = () => {
+//   // Your Quote component logic here
+//   return <div>Quote Component</div>;
+// };
+
 import axios from "axios";
 import { useState } from "react";
 
-const useQuote = () => {
+export const useQuote = () => {
   const [quote, setQuote] = useState(null);
+  const [error, setError] = useState(null);
 
   const fetchQuote = async () => {
     try {
@@ -13,7 +60,11 @@ const useQuote = () => {
       setError("Error fetching quote");
     }
   };
+
   return { quote, fetchQuote };
 };
 
-export default useQuote;
+export const Quote = () => {
+  // Your Quote component logic here
+  return <div>Quote Component</div>;
+};
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,32 @@
+// "use client";
+// import React, { createContext, useContext, useEffect } from "react";
+// import useQuote from "./Quote/quote";
+// import Dashboard from "./Dashboard/dashboard";
+// import Data from "./data";
+
+// export const QuoteContext = createContext();
+
+// export default function Home() {
+//   const { quote, fetchQuote } = useQuote();
+
+//   useEffect(() => {
+//     fetchQuote();
+//   }, [fetchQuote]);
+
+//   return (
+//     <main className="">
+//       <QuoteContext.Provider value={quote}>
+//         <Data />
+//       </QuoteContext.Provider>
+//       <Dashboard />
+//     </main>
+//   );
+// }
+"use client";
 import React, { createContext, useContext, useEffect } from "react";
-import Quote from "./Quote/quote";
+import { useQuote, Quote } from "./Quote/quote";
 import Dashboard from "./Dashboard/dashboard";
+import Data from "./data";
 
 export const QuoteContext = createContext();
 
@@ -13,7 +39,9 @@ export default function Home() {
 
   return (
     <main className="">
-      <QuoteContext.Provider value={quote}>{Quote}</QuoteContext.Provider>
+      <QuoteContext.Provider value={quote}>
+        <Data />
+      </QuoteContext.Provider>
       <Dashboard />
     </main>
   );
