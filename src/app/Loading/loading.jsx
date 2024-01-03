521473a42afdddd5f6eba582da7cbadaa43cfe90
Ego Nwaekpe <glorynwaekpe@gmail.com>
2024-01-03 06:48:28 -0800

    added trivail api

---
+++ src/app/Loading/loading.jsx
@@ -0,0 +1,12 @@
+const Loading = () => {
+  return (
+    <>
+      <div className="spinner">Loading</div>
+      <div className="spinner-circle spinner-red"></div>
+      <div className="spinner-circle spinner-blue"></div>
+      <div className="spinner-circle spinner-yellow"></div>
+    </>
+  );
+};
+
+export default Loading;
