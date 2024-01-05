25a9f7ebd4c3ec1972710bef889c81fbfe470afb
Ego Nwaekpe <glorynwaekpe@gmail.com>
2024-01-05 08:58:18 -0800

    adding random on the image

--- src/app/Modal/modal.jsx
+++ src/app/Modal/modal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "react-modal";
 import Spinner from "../loading/page";
 
@@ -14,6 +14,34 @@ const ReactModal = ({
   loading,
   Imgs,
 }) => {
+  const [selectImage, setSelectImage] = useState();
+
+  // const selectedImg = () => {
+  //   const matchingImage = Imgs.find((img) => img.id === selectedData.id);
+  //   const selectedImage = matchingImage?.image;
+  //   const randomImage = Math.random(selectImage)
+
+  //   setSelectImage(randomImage);
+  // };
+
+  const selectedImg = () => {
+    const matchingImages = Imgs.filter((img) => img.id === selectedData.id);
+
+    if (matchingImages.length > 0) {
+      const randomIndex = Math.floor(Math.random() * matchingImages.length);
+
+      const randomImage = matchingImages[randomIndex];
+
+      const selectedImage = randomImage?.image || "default_image_url";
+
+      setSelectImage(selectedImage);
+    } else {
+      setSelectImage("default_image_url");
+    }
+  };
+
+  console.log(selectImage);
+
   const customStyles = {
     content: {
       top: "50%",
@@ -26,6 +54,8 @@ const ReactModal = ({
       height: "300px",
     },
   };
+
+  // const santa = Imgs.find((img) => img.id === selectedData.id)?.image;
 
   return (
     <div>
@@ -41,10 +71,13 @@ const ReactModal = ({
             <>
               {/* {selectedData && <img src={selectedData.image} />} */}
               {/* {selectedData && <img src={Imgs} alt="Selected Image" />} */}
+
               {selectedData && (
                 <img
-                  src={Imgs.find((img) => img.id === selectedData.id)?.image}
+                  // src={Imgs.find((img) => img.id === selectedData.id)?.image}
+                  src={selectedData?.selectImage}
                   alt="Selected Image"
+                  className="w-[300px] h-[250px] modal-Img"
                 />
               )}
               {(selectedData?.id === 2 ||
